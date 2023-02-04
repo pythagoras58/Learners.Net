ed97905be8d53f8427348bf0711d75d960351a69
pythagoras58 <solomondanso58@gmail.com>
2023-02-04 23:10:10 +0000

    DATA STRUCT

--- Learner.Tutorials/Program.cs
+++ Learner.Tutorials/Program.cs
@@ -1,5 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace Learner.Tutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pythagoras");
+            
+            // variables:
+            string myString = "Pythagoras";
+            decimal myDecimal = 12.23M;
+            bool myBool = true;
+            
+            Console.WriteLine(myString.GetType());
+            Console.WriteLine(myDecimal.GetType());
+            Console.WriteLine(myBool.GetType());
+            
+            // Data Structures
+            Console.WriteLine("Data Structure");
+            string[] food = new string[2];
+            food[0] = "Banku";
+            food[1] = "Tilapia";
 
-Console.WriteLine("Hello, World!");
-Console.Write("First");
-Console.Write("Second");
+            foreach (var foodItem in food)
+            {
+                Console.WriteLine(foodItem);
+            }
+            
+        }
+    }
+}
